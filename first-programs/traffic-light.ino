40f82d383e58d578c74c975a406ea4eecde0e30b
Matthew Bickell <matthewbickell@hotmail.com>
2026-01-09 10:58:21 +0200

    cleaner code

--- first-programs/traffic-light.ino
+++ first-programs/traffic-light.ino
@@ -2,27 +2,36 @@
 /*	This is a default program--
 	Use File->Load Prog to load a different program
 */
+#define RED_LIGHT 2
+#define YELLOW_LIGHT 3
+#define GREEN_LIGHT 4
+
+#define DELAY 1000
 
 void setup()
 {
-	pinMode(2, OUTPUT);
-	pinMode(3, OUTPUT);
-	pinMode(4, OUTPUT);
+	pinMode(RED_LIGHT, OUTPUT);
+	pinMode(YELLOW_LIGHT, OUTPUT);
+	pinMode(GREEN_LIGHT, OUTPUT);
+	
+	for(int i = 2; i < 5; i++) {
+		digitalWrite(i, LOW);
+	}
 }
 
 void loop()
 {
-	digitalWrite(2, HIGH);
-	delay(2000);
-	digitalWrite(3, HIGH);
-	delay(1000);
-	digitalWrite(2, LOW);
-	digitalWrite(3, LOW);
-	digitalWrite(4, HIGH);
-	delay(2000);
-	digitalWrite(3, HIGH);
-	digitalWrite(4, LOW);
-	delay(1000);
-	digitalWrite(2, HIGH);	
-	digitalWrite(3, LOW);
+	digitalWrite(RED_LIGHT, HIGH);
+	delay(DELAY * 2);
+	digitalWrite(YELLOW_LIGHT, HIGH);
+	delay(DELAY);
+	digitalWrite(RED_LIGHT, LOW);
+	digitalWrite(YELLOW_LIGHT, LOW);
+	digitalWrite(GREEN_LIGHT, HIGH);
+	delay(DELAY * 2);
+	digitalWrite(YELLOW_LIGHT, HIGH);
+	digitalWrite(GREEN_LIGHT, LOW);
+	delay(DELAY);
+	digitalWrite(RED_LIGHT, HIGH);	
+	digitalWrite(YELLOW_LIGHT, LOW);
 }
