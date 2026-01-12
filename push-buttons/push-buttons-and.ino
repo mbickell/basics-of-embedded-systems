f965676ae54e4ec00d9cbc187ce817bf7c7d0863
Matthew Bickell <matthewbickell@hotmail.com>
2026-01-12 10:15:10 +0200

    push buttons

---
+++ push-buttons/push-buttons-and.ino
@@ -0,0 +1,30 @@
+
+/*	This is a default program--
+	Use File->Load Prog to load a different program
+*/
+
+char buttonOne = 2;
+char buttonTwo = 3;
+char led = 12;
+
+char valueOne = 0;
+char valueTwo = 0;
+char ledValue = 0;
+
+void setup()
+{
+	pinMode(led, OUTPUT);
+	pinMode(buttonOne, INPUT);
+	pinMode(buttonTwo, INPUT);
+}
+
+void loop()
+{
+	valueOne = digitalRead(buttonOne);
+	valueTwo = digitalRead(buttonTwo);
+
+	// ledValue = valueOne || valueTwo;
+	ledValue = valueOne && valueTwo;
+
+	digitalWrite(led, ledValue);
+}
