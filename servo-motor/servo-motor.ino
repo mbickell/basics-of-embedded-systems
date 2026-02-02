1598505c246603e071c4445513f20989c5c0f7ef
Matthew Bickell <matthewbickell@hotmail.com>
2026-02-02 11:22:29 +0200

    finish servo traffic light program

--- servo-motor/servo-motor.ino
+++ servo-motor/servo-motor.ino
@@ -1,40 +1,98 @@
 #include <Servo.h>
 
 Servo myservo; // create servo object to control a servo
-// twelve servo objects can be created on most boards
+
+// Used pins
+byte motor = 7;
+byte redLight = 12;
+byte yellowLight = 11;
+byte greenLight = 10;
+byte boomLightRed = 9;
+byte boomLightGreen = 8;
+byte button = 2;
+
+// Globals
 const int min = 600;
-const int max = 2400;
-const int mid = (max + min) / 2;
-int pos = mid; // variable to store the servo position
+const int max = 1500;
+int pos = min; // variable to store the servo position
+int lightDelayMS = 1500;
 
-void setup() {
-	myservo.attach(9); // attaches the servo on pin 9 to the servo object
-}
+volatile byte state = LOW;  // variable that will be updated in the ISR
 
-void loop() {
-	// for (pos = min; pos <= max; pos += 1) { // goes from 0 degrees to 180 degrees
-	// // in steps of 1 degree
-	// myservo.writeMicroseconds(pos); // tell servo to go to position in variable 'pos'
-	// delay(15); // waits 15ms for the servo to reach the position
-	// }
+void liftBoom() {
+	while (pos < max)
+	{
+		myservo.writeMicroseconds(pos);
+		pos += 2;
+		delay(10);
+	}
 
-	// for (pos = max; pos >= min; pos -= 1) { // goes from 180 degrees to 0 degrees
-	// myservo.writeMicroseconds(pos); // tell servo to go to position in variable 'pos'
-	// delay(15); // waits 15ms for the servo to reach the position
-	// }
+	updateBoomLights();
+
+	delay(lightDelayMS * 2);
+	
+	state = LOW;
+	updateBoomLights();
 
 	while (pos > min)
 	{
 		myservo.writeMicroseconds(pos);
-		pos--;
+		pos -= 2;
 		delay(10);
 	}
+}
 
-	while (pos < max)
-	{
-		myservo.writeMicroseconds(pos);
-		pos++;
-		delay(10);
+void resetTrafficLights() {
+	digitalWrite(redLight, HIGH);
+	digitalWrite(yellowLight, LOW);
+	digitalWrite(greenLight, LOW);
+}
+
+void runTrafficLights() {
+	resetTrafficLights();
+	delay(lightDelayMS * 2);
+	digitalWrite(yellowLight, HIGH);
+	delay(lightDelayMS);
+	digitalWrite(redLight, LOW);
+	digitalWrite(yellowLight, LOW);
+	digitalWrite(greenLight, HIGH);
+	delay(lightDelayMS * 2);
+	digitalWrite(greenLight, LOW);
+	digitalWrite(yellowLight, HIGH);
+	delay(lightDelayMS);
+	resetTrafficLights();
+}
+
+void setBoomLight() {
+	state = HIGH;
+}
+
+void updateBoomLights() {
+	digitalWrite(boomLightRed, !state);
+	digitalWrite(boomLightGreen, state);
+}
+
+void setup() {
+	myservo.attach(motor); // attaches the servo on pin 9 to the servo object
+
+	for(byte i = 8; i < 13; i++) {
+		pinMode(i, OUTPUT);
 	}
 
+	pinMode(button, INPUT);
+
+	resetTrafficLights();
+	updateBoomLights();
+
+	attachInterrupt(digitalPinToInterrupt(button), setBoomLight, RISING);
+	
+	myservo.writeMicroseconds(min);
+}
+
+void loop() {
+	runTrafficLights();
+
+	if (state == HIGH) {
+		liftBoom();
+	}
 }
